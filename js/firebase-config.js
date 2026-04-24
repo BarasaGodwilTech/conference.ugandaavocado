0f774ad713bf1d939a3dee4413d6f2bc464a1f28
barasalivingstone <barasagodwil@gmail.com>
2026-04-24 17:41:03 +0300

    gjc

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -5,15 +5,15 @@ import { getAuth, onAuthStateChanged, signInWithEmailAndPassword, createUserWith
 import { getFunctions, httpsCallable } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-functions.js";
 
 // Firebase configuration
-
 const firebaseConfig = {
   apiKey: "AIzaSyDB0vJdDle5OFRPlSroj9-fGcg17t1_Dno",
   authDomain: "app.ugandaavocados.org",
   projectId: "avocado-conference",
   storageBucket: "avocado-conference.firebasestorage.app",
   messagingSenderId: "926227966647",
-  appId: "1:926227966647:web:6618ccc5e2f38523783b32"
+  appId: "1:926227966647:web:26bfae5851476743783b32"
 };
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
